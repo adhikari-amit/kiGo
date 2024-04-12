bcd3271a7c4cd1bf2948b34a109be243bf6fed3f
adhikari-amit <adhikariamit2001@gmail.com>
2024-04-12 17:11:26 +0530

    GO Routine using sync Std. Library.

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSome(s string, wg *sync.WaitGroup) {
 	fmt.Println(s)
+	defer wg.Done()
 }
 
+// A goroutine is a lightweight thread managed by the Go runtime
 func main() {
-	// A goroutine is a lightweight thread managed by the Go runtime
+
+	// A Wait Group using Go "sync" std library
+	var wg sync.WaitGroup
 	words := []string{
 		"alpha",
 		"beta",
@@ -19,17 +23,22 @@ func main() {
 		"phi",
 		"che",
 		"zeta",
+		"theta",
+	}
+	// Adding length for that time wait group has to wait
+	wg.Add(len(words))
+	for i, word := range words {
+		// Init a thred by using "go" keyword infront of the command
+
+		go printSome(fmt.Sprintf("%s is at %d", word, i), &wg)
 	}
 
-	for _, word := range words {
-		// Init a thred by using "go" keyword infront of the command
-		go fmt.Println(word)
-	}
+	wg.Wait()
 
 	// Introducting a 1 sec sleep just to wait for returning the result from the threads
-	time.Sleep(1 * time.Second)
+	// time.Sleep(1 * time.Second) This is a bad approach to waiting for 1sec
 
-	printSomething("amamit")
+	fmt.Printf("amamit")
 	// Observe the output. This is due to not managing the threads we created in the go
 
 }
