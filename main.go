748648a84bd24b2e8b2c63d58ccc9d242a72b868
ama3it <adhikariamit2001@gmail.com>
2024-04-15 17:00:14 +0530

    GO fiber config

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"GO200/channel"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
 )
 
+const PORT = ":8080"
+
 func main() {
-	channel.Channel()
+	app := fiber.New()
+
+	app.Get("/api/list", func(c fiber.Ctx) error {
+		return c.SendString("I'm a GET request!")
+	})
+
+	app.Post("/api/register", func(c fiber.Ctx) error {
+		return c.SendString("I'm a POST request!")
+	})
+
+	log.Fatal(app.Listen(PORT))
 }
